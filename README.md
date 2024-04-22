6a2bdec2bf03684744e52cbfa1a61dbd5cedd6b6
j m <jtmcnamara@proton.me>
2024-04-23 09:29:38 +1000

    More testing with serial

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Pam
-
+# Pam GUI
 ## 1. Installation
 Currently using [eBUS SDK](https://www.jai.com/support-software/ubuntu-x86) for JAI version 6.4 for ubuntu 22.04 x86-64bit. The quickstart guide for linux in the above link has info for installation. Before installing eBUS SDK the following packages are required:
 1. sudo apt-get install libavcodec58
@@ -51,3 +50,32 @@ Where 9000 indicates a 9000 byte frame.
 ## 5. Application
 1. The aqcuisition rate of the camera is an attribute that can be changed. However, actual received FPS may be different from the selected acquisition rate. The actual frame rate can be improved by reducing the image width/height settings which allows much higher frame rates. Changing the height/width does not scale the image but crops it.
 2. For low light applications, the pixel binning option can improve the camera sensitivity significanly. The outcome is a brighter image at half the resolution and a greater possible frame rate.
+
+# AVR Controller
+The AVR controller is based on the AVR64DA32 chip which is programmed via the UPDI interface.
+
+## 1. Compiling
+The C/C++ code is compiled using avr-gcc/avr-g++ and makes use of avr-libc. [Microchip Studio](https://www.microchip.com/en-us/tools-resources/develop/microchip-studio) was used as the IDE as it integrates the avr-g++ toolchain so building is easy.
+
+## 2. Uploading
+The serialUPDI interface was used to upload .hex or .elf files to the chip with the help of [avrdude](https://github.com/avrdudes/avrdude). An avrdude upload command can be added to Microchip Studio as a external tool to make uploading executables easier.
+Go to Tools->External Tools and add a new tool. The tool was configured as:
+
+Command:
+```
+C:\Program Files\avrdude-v7.3-windows-x64\avrdude.exe
+```
+Arguments:
+```
+-p avr64da32 -P COM5 -b 115200 -c serialupdi -C "C:\Program Files\avrdude-v7.3-windows-x64\avrdude.conf" -U flash:w:$(BinDir)\led-controller.hex:i
+```
+
+Initial Directory:
+```
+$(BinDir)
+```
+
+In order to set fuses with avrdude on the newer DA series chips, it was necessary to manually write it. Luckily the corrent fuse locations are specified in avrdude.conf under the DX family information. To enable the reset pin, the fuse was written by:
+```
+avrdude -c serialupdi -p avr64da32 -P com5 -U syscfg0:w:0xC8:m
+```
--- serial-controller/src/led-controller/led-controller/main.cpp
+++ serial-controller/src/led-controller/led-controller/main.cpp
@@ -6,11 +6,13 @@
  */ 
 
 // Clock params
-#define F_CPU 24000000
+#define F_CPU 8000000UL
 
 // USART 2 params
 #define USART_BAUD_RATE(BAUD_RATE) ((float)(4000000 * 64 / (16 * (float)BAUD_RATE)) + 0.5)
-#define MAX_INPUT_LEN 36
+#define MAX_INPUT_LEN 128
+#define MAX_NUM_COMMANDS 32
+#define MAX_NUM_INTS 3*MAX_NUM_COMMANDS
 
 // SPI params
 
@@ -21,15 +23,68 @@
 #include <util/delay.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <errno.h>
+
+// These are the specific actions that
+enum Action
+{
+	WRITE_DAC_A,
+	WRITE_DAC_B,
+	LED_SHORT_ON,
+	LED_SHORT_OFF,
+	LED_ON,
+	LED_OFF,
+	LED_WHITE_ON,
+	LED_WHITE_OFF,
+	TRIGGER_HIGH,
+	TRIGGER_LOW
+};
+
+struct Command
+{
+	int action;
+	uint32_t time;
+	uint16_t value;
+};
+
+
+void writeDACn(uint16_t n, uint16_t value)
+{
+	value = value > 4096 ? 4096 : value;
+
+	// Add configuration bits for DAC selection (a/b) and normal operation
+	value |= (n >= 0x01) << 15 | 0x01 << 13 | 0x01 << 12;
+
+	// Send SPI
+}
+
+void shutdownDACn(uint16_t n)
+{
+	// Add configuration bits for DAC selection (a/b) and the shutdown bit
+	uint16_t value = (n >= 0x01) << 15 & ~(0x01 << 12);
+
+	value++;
+}
 
 void CLOCK_init()
 {
-	// Set peripheral clock = fclk / 6 = 4MHz
-	CLKCTRL.MCLKCTRLB |= CLKCTRL_PDIV_6X_gc | CLKCTRL_PEN_bm;
+	_PROTECTED_WRITE(CLKCTRL.OSCHFCTRLA, CLKCTRL_FRQSEL_8M_gc);
+	//_PROTECTED_WRITE(CLKCTRL.XOSC32KCTRLA,  CLKCTRL_ENABLE_bm);
+	_PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, CLKCTRL_PEN_bm);
+	_PROTECTED_WRITE(CLKCTRL.MCLKCTRLA, CLKCTRL_CLKSEL_OSCHF_gc);
+}
+
+void USART2_sendChar(char c)
+{
+	// Wait until transmit data register is empty
+	while (!(USART2.STATUS & USART_DREIF_bm))
+	{
+		;
+	}
 	
-	// Set main clock to internal oscillator @ 24MHz with auto tune enabled
-	CLKCTRL.OSCHFCTRLA |= CLKCTRL_FRQSEL_24M_gc | CLKCTRL_AUTOTUNE_bm;
+	// Send data
+	USART2.TXDATAL = c;
 }
 
 // Initialize USART2 in their default pin locations
@@ -46,7 +101,7 @@ void USART2_init(uint16_t baud)
 	
 	// Set asynchronous, no parity, 8bit data, 1 stop bit
 	// This is the default configuration but we set it explicitly for clarity
-	USART2.CTRLC |= USART_CMODE0_bm | USART_PMODE0_bm | USART_SBMODE_1BIT_gc | USART_CHSIZE_8BIT_gc;
+	USART2.CTRLC |= USART_PMODE0_bm | USART_SBMODE_1BIT_gc | USART_CHSIZE_8BIT_gc;
 }
 
 void SPI0_init()
@@ -55,7 +110,7 @@ void SPI0_init()
 	PORTA.DIR |= PIN4_bm | PIN6_bm | PIN7_bm;
 	
 	// Set endianness, master mode, /4 prescale, no clock doubling, SPI enable
-	SPI0.CTRLA = SPI_DORD_bm | SPI_MASTER_bm & ~SPI_CLK2X_bm | SPI_PRESC_DIV4_gc | SPI_ENABLE_bm;
+	SPI0.CTRLA = SPI_DORD_bm | SPI_MASTER_bm & (~SPI_CLK2X_bm) | SPI_PRESC_DIV4_gc | SPI_ENABLE_bm;
 }
 
 uint8_t SPI0_write(uint8_t data)
@@ -84,23 +139,6 @@ uint8_t USART2_readChar()
 	return USART2.RXDATAL;
 }
 
-void USART2_readString(void)
-{
-	
-}
-
-void USART2_sendChar(char c)
-{
-	// Wait until transmit data register is empty
-	while (!(USART2.STATUS & USART_DREIF_bm))
-	{
-		;
-	}
-	
-	// Send data
-	USART2.TXDATAL = c;
-}
-
 void USART2_sendString(char *str)
 {
 	for(size_t i = 0; i < strlen(str); i++)
@@ -109,47 +147,19 @@ void USART2_sendString(char *str)
 	}
 }
 
-void parse(char* cmd)
+void USART2_readString(char* str)
 {
-	/* Command format will be [t1, a1, t2, a2, t3, a3, ...] where
-	 * tn -> time in microseconds
-	 * an -> action to take at that time
-	 * times are relative to message receipt
-	 */
-	if(*cmd != '[')
-	{
-		USART2_sendString("Bad input!");
-		return;
-	}
-	
-	for(;;)
-	{
-		errno = 0;
-		char* end;
-		
-		// Extract number
-		const long i = strtol(cmd, &end, 10);
-	}
-}
-
-
-int main(void)
-{
-	CLOCK_init();
-	USART2_init(9600);
-	
-	// USART message vars
-	char input[MAX_INPUT_LEN];
+	USART2_sendString("Started reading\n");
 	char c;
 	uint8_t index = 0;
-	
-    while (1) 
-    {
+
+	while (1)
+	{
 		c = USART2_readChar();
 		if(c != '\n' && c != '\r')
 		{
-			input[index++] = c;
-	
+			str[index++] = c;
+		
 			// overwrite old data if it overruns the input buffer
 			if(index > MAX_INPUT_LEN)
 			{
@@ -159,10 +169,92 @@ int main(void)
 
 		if(c == '\n')
 		{
-			input[index] = '\0';
+			str[index] = '\0';
 			index = 0;
-			parse(input);
+			USART2_sendString("char!\n");
+			return;
 		}
-    }
+	}
+}
+
+
+
+bool parse(const char* c, char** end, Command* cmds, size_t& idx)
+{
+	//printf("Parsing '%s':\n", c);
+	uint32_t buffer[MAX_NUM_INTS];
+	uint8_t n = 0;
+
+	USART2_sendString("Started parsing\n");
+	while (n < MAX_NUM_INTS)
+	{
+		// Reset errno, as its used by many std functions
+		errno = 0;
+
+		// Attempt to extract a long from the string
+		const uint32_t val = strtoul(c, end, 10);
+
+		// Check if there was a range error
+		const bool range_error = errno == ERANGE;
+
+		// Conversion failed, stop
+		if (c == *end || range_error)
+		break;
+
+		// Update the start of the string to where the last number extracted finished
+		c = *end;
+
+		// Add the new number to the buffer
+		buffer[n] = val;
+
+		// Increment counter
+		n++;
+	}
+
+	// Need multiple of 3 to make valid commands
+	if (n % 3 != 0)
+	{
+		return false;
+	}
+
+	//
+	idx = n / 3;
+	for(int i = 0; i < n; i += 3)
+	{
+		cmds[i/3] = {int(buffer[i]), buffer[i+1], uint16_t(buffer[i+2]) };
+	}
+
+	return true;
+}
+
+
+int main(void)
+{
+	CLOCK_init();
+	USART2_init(9600);
+			
+	char buffer[MAX_INPUT_LEN];
+	Command commands[MAX_NUM_COMMANDS];
+
+	_delay_ms(30);
+	USART2_sendString("Rebooted\n");
+
+	while (1)
+	{
+		// Wait for a line
+		USART2_readString(buffer);
+
+		char* end;
+		size_t idx = 0;
+		if ( parse(buffer, &end, commands, idx) )
+		{
+			USART2_sendString("Good parse!\n");
+		}
+		else
+		{
+			USART2_sendString("Bad parse!\n");
+			USART2_sendString(end);
+		}
+	}
 }
 
